59904ee729b262b38363a33b388868949a21d24e
Ajith7736 <ajith.aju39502@gmail.com>
2025-09-26 21:42:50 +0530

    Added google and github nextauth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
