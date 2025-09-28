5aa35c5b200c1847a56c2841011e7db3ee906477
Ajith7736 <ajith.aju39502@gmail.com>
2025-09-28 20:31:20 +0530

    Home page ui completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,26 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
+import { Geist } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import SessionWrapper from "@/components/SessionWrapper";
 import NavbarWrapper from "@/components/NavbarWrapper";
 
-// Inter (body font)
+
 const inter = Inter({
   subsets: ["latin"],
-  display: "swap",
+  variable: "--font-body",
+  display: "swap"
 });
 
-// Geist Sans (headings)
-const geistSans = Geist({
-  variable: "--font-heading",
-  subsets: ["latin"],
-});
+const geistSans = Geist({ variable: "--font-heading", subsets: ["latin"] });
+const geistMono = Geist_Mono({ variable: "--font-mono", subsets: ["latin"] });
 
-// Geist Mono (code/mono text)
-const geistMono = Geist_Mono({
-  variable: "--font-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "GenUI",
-  description: "An Web app for ui designs",
+  description: "A Web app for ui designs",
 };
 
 export default function RootLayout({
@@ -35,10 +30,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <SessionWrapper>
+      <body className={`${inter.className} ${geistSans.className} ${geistMono.className} antialiased`} >
+        <SessionWrapper >
           <Toaster position="top-right" reverseOrder={false} />
           <NavbarWrapper />
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,115 @@
 "use client"
+import React, { useState } from "react";
+import { BsLightningChargeFill } from "react-icons/bs";
+import { PiEyesFill } from "react-icons/pi";
+import { FaLaptopCode } from "react-icons/fa";
+import { IoIosColorPalette } from "react-icons/io";
+import { IoLibrary } from "react-icons/io5";
+import { FaHandshakeSimple } from "react-icons/fa6";
+import { IoSunnyOutline } from "react-icons/io5";
+import { IoMoonOutline } from "react-icons/io5";
+import { motion, AnimatePresence } from "framer-motion";
 
-import { string } from "zod";
+
 
 export default function Home() {
+  const [showtoggle, setshowtoggle] = useState(false)
+
   interface Card {
-    logo: string,
+    logo: React.ReactElement,
     title: string,
     description: string
   }
 
   const featurecard: Card[] = [
     {
-      logo: "",
-      title: "⚡ Text to UI",
+      logo: <BsLightningChargeFill size={35} />,
+      title: "Text to UI",
       description: "Describe your idea and generate UI instantly.",
     },
     {
-      logo: "",
-      title: "👀 Live Preview",
+      logo: <PiEyesFill size={35} />,
+      title: "Live Preview",
       description: "See your UI before exporting the code.",
     },
     {
-      logo: "",
-      title: "💻 Code Export",
+      logo: <FaLaptopCode size={35} />,
+      title: "Code Export",
       description: "Copy or download clean React + Tailwind code.",
     },
     {
-      logo: "",
-      title: "🎨 Customizable Components",
+      logo: <IoIosColorPalette size={35} />,
+      title: "Customizable Components",
       description: "Edit styles, colors, and layouts with ease.",
     },
     {
-      logo: "",
-      title: "📚 Templates Library",
+      logo: <IoLibrary size={34} />,
+      title: "Templates Library",
       description: "Start quickly with dashboards, forms, and more.",
     },
     {
-      logo: "",
-      title: "🤝 Collaboration",
+      logo: <FaHandshakeSimple size={35} />,
+      title: "Collaboration",
       description: "Share your generated UI/code with teammates.",
     }
   ]
 
+  const showToggle = () => {
+    setshowtoggle(!showtoggle);
+  }
+
 
   return (
     <div>
-      <div className="flex flex-col items-center py-10 px-10 gap-5 font-inter md:h-[50vh] lg:h-[70vh] lg:justify-center xl:h-[90vh]  xl:px-80 xl:gap-10">
-        {/* section 1 */}
+
+
+      <section className="flex flex-col items-center py-10 px-10 gap-5 font-inter md:h-[50vh] lg:h-[70vh] lg:justify-center xl:h-[90vh]  xl:px-80 xl:gap-10">
         <div className="bg-light-mediumgrey md:text-xl py-2 px-4 rounded-full font-medium">This is your UI partner</div>
-        <div className="text-3xl md:text-5xl text-center font-bold font-heading xl:text-6xl">An Generative UI Builder Powered By groq.</div>
+        <div className="text-3xl md:text-5xl text-center font-bold font-heading xl:text-6xl">A Generative UI Builder Powered By groq.</div>
         <div className="text-light-darkgrey md:text-2xl text-center text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, libero sint necessitatibus architecto nulla officia earum vero magni? Ad, debitis.</div>
         <div className="flex gap-3">
           <button className="text-light-white bg-light-black px-5 py-3 rounded-md cursor-pointer hover:bg-light-black/90 transition-all ease-in-out">Get Started</button>
-          <button className="text-light-black bg-light-white border border-light-grey hover:bg-light-grey/20 transition-all ease-in-out cursor-pointer px-5 py-3 rounded-md">Github</button>
+          <a href="https://github.com/Ajith7736/GenUI"><button className="text-light-black bg-light-white border border-light-grey hover:bg-light-grey/20 transition-all ease-in-out cursor-pointer px-5 py-3 rounded-md">Github</button></a>
         </div>
-      </div>
-      {/* section 2 */}
-      <div className="bg-light-mediumgrey/60 w-full min-h-[80vh] flex flex-col items-center py-5 px-10 gap-8">
+      </section>
+
+
+      <section className="bg-light-mediumgrey/60 w-full min-h-[80vh] flex flex-col items-center py-5 px-10 gap-8">
         <div className="text-3xl font-bold font-heading">Features</div>
         <div className="text-light-darkgrey text-center text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, libero sint necessitatibus architecto nulla officia earum vero magni? Ad, debitis.</div>
 
         {featurecard.map((item, index) => {
-          return <div className="bg-light-white w-full min-h-[25vh] rounded-xl border border-light-grey p-10 flex flex-col gap-5 justify-center">
-            <div className="text-2xl font-bold">Logo</div>
+          return (<div key={index} className="bg-light-white w-full min-h-[25vh] rounded-xl border border-light-grey p-10 flex flex-col gap-5 justify-center">
+            <div className="text-2xl font-bold">{item.logo}</div>
             <div className="text-lg font-medium">{item.title}</div>
-            <div className="font-medium text-light-darkgrey">{item.}</div>
-          </div>
+            <div className="font-medium text-light-darkgrey">{item.description}</div>
+          </div>)
         })}
 
-      </div>
+        <div className="text-center text-light-darkgrey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor inventore nobis eos minima ipsam dicta corrupti ratione incidunt quo omnis?</div>
+      </section>
 
+      <section className="p-10 flex relative flex-col items-center gap-5 select-none">
+        <div className="text-center text-3xl font-bold">Proudly Open Source</div>
+        <div className="text-center text-light-darkgrey">GenUI is open source and powered by open source software.
+          The code is available on <a href="" className="underline">GitHub</a>.</div>
+        <div className="text-center mt-20">Built by <a href="https://nextjs.org/" className="underline">Next.js</a> , Hosted on <a href="https://vercel.com/" className="underline">vercel</a> , Inspired by <a href="https://tx.shadcn.com/" className="underline">Taxonomy</a> .The source code is available on <a href="" className="underline">GitHub</a> </div>
+        <AnimatePresence>
+          {showtoggle && (
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 20 }}
+              transition={{ duration: 0, ease: "easeInOut", delay: 0 }}
+              className="bg-light-white transition-all ease-in-out duration-400 absolute bottom-25  h-fit w-[100px] rounded-xl shadow-md select-none">
+              <div className="flex items-center gap-2 p-1 hover:bg-light-mediumgrey rounded-t-xl transition-all ease-in-out duration-200 cursor-pointer"><IoSunnyOutline />Light</div>
+              <div className="flex items-center gap-2 p-1 hover:bg-light-mediumgrey cursor-pointer transition-all ease-in-out duration-200"><IoMoonOutline />Dark</div>
+              <div className="flex items-center gap-2 p-1 hover:bg-light-mediumgrey cursor-pointer transition-all ease-in-out duration-200"><FaLaptopCode />System</div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <IoSunnyOutline size={40} className="mt-6 cursor-pointer transition-all ease-in-out hover:bg-light-mediumgrey p-2 rounded-xl" onClick={showToggle} />
+      </section>
     </div>
   );
 }
